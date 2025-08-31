d236a8e8debc2fb8a4d35483359137d05da4fa4f
fujimmm331 <nyappy15th@gmail.com>
2025-08-31 15:05:06 +0900

    fix: timeoutの設定

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  timeout: 60 * 1000,
   use: {
     baseURL: 'https://tickets.fcbayern.com',
     trace: 'on-first-retry',
